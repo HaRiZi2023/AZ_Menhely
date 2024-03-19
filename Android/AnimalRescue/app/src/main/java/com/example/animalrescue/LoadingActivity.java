a95fe356ea54d2dd60fde8556a08bb33b89efb12
husibaby <hanyat92@gmail.com>
2024-03-19 22:32:59 +0100

    #2 - Startpage/ProgressBar ID's

---
+++ Android/AnimalRescue/app/src/main/java/com/example/animalrescue/LoadingActivity.java
@@ -0,0 +1,35 @@
+package com.example.animalrescue;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.window.SplashScreen;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT=5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_loading);
+        new Handler().postDelayed(()-> {
+            Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        },SPLASH_TIME_OUT);
+    }
+
+
+}
