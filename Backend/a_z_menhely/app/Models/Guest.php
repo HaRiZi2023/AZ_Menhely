5bfd1b0cc6fcc2ac923756bec0b98293e9bb8e4b
Szebeni Zita <mazsola.muvek@gmail.com>
2024-04-10 14:19:36 +0200

    1. Guest, Worker -migráció módosítás- softdeletes, 2. Adoption, Guest, Worker request készítés, 3. Worker faktori - módosítás -permission

--- Backend/a_z_menhely/app/Models/Guest.php
+++ Backend/a_z_menhely/app/Models/Guest.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Guest extends Model
 {
-    use HasFactory;
+    use SoftDeletes, HasFactory;
 
     protected $table = 'guests';
     protected $fillable = ['g_name', 'g_chip', 'g_species', 'g_gender', 'g_in_date', 'g_in_place' , 'g_out_date', 'g_adoption', 'other', 'g_image'];
