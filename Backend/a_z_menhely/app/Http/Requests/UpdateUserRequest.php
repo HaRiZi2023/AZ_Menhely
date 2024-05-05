09f18057b77de9a65f1cfd8000f3608c15c4fe7c
Magyarosi Richárd <richard.magyarosi@gmail.com>
2024-05-06 00:49:24 +0200

    a felhasználók így képesek frissíteni a profiljukat ha true az authorize() függvény

--- Backend/a_z_menhely/app/Http/Requests/UpdateUserRequest.php
+++ Backend/a_z_menhely/app/Http/Requests/UpdateUserRequest.php
@@ -11,7 +11,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,10 +23,10 @@ class UpdateUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:100',
-            //'email' => 'required|string|max:100',
+            'email' => 'required|string|max:100',
             'password' => 'required|string|max:100',
-            //'address'=>'required|string|max:100',
-            //'phone'=>'required|string|min:8|max:20',
+            'address'=>'required|string|max:100',
+            'phone'=>'required|string|min:8|max:20',
             'role' => 'required|in: admin, worker, user'
         ];
     }
