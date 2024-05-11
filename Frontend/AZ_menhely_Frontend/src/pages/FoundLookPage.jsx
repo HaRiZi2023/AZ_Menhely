320444741120780a4964bd6063e492d6fa8f2161
Magyarosi Richárd <richard.magyarosi@gmail.com>
2024-05-11 15:43:10 +0200

    col-lg-5 col-md-8 col-sm-12 mb-4 osztályok hogy a kártyák a megfelelően méreteződjenek justify-content-center osztály középre igazítja a kártyát

--- Frontend/AZ_menhely_Frontend/src/pages/FoundLookPage.jsx
+++ Frontend/AZ_menhely_Frontend/src/pages/FoundLookPage.jsx
@@ -5,22 +5,20 @@ import image2 from '../assets/front4.jpg';
 
 function FoundLookPage() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '45px' }}>
-      <div>
+    <div className="row justify-content-center" style={{ padding: '45px' }}>
+      <div className="col-lg-5 col-md-8 col-sm-12 mb-4">
         <h2 className="my-2 ">Talált állatok</h2>
         <Link to="/look-for">
           <div className={styles.card}>
-          <img
-              className={styles['card-img-top']} src={image1} alt="Fourth slide" />
+            <img className={styles['card-img-top']} src={image1} alt="Fourth slide" />
           </div>
         </Link>
       </div>
-      <div>
+      <div className="col-lg-5 col-md-8 col-sm-12 mb-4">
         <h2 className="my-2 ">Elveszett állatok</h2>
         <Link to="/found">
           <div className={styles.card}>
-          <img
-              className={styles['card-img-top']} src={image2} alt="Fourth slide" />
+            <img className={styles['card-img-top']} src={image2} alt="Fourth slide" />
           </div>
         </Link>
       </div>
